f95d3bce01602b5a6d42e5956da6fb9bfb08db8d
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2026-05-30 05:22:31 +0300

    Writing messages with SWV console

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
 
+#include "stdio.h" // for printf
+
 #include "FreeRTOS.h"
 #include "task.h"
+
+// For printf
+int _write(int32_t file, uint8_t *ptr, int32_t len)
+{
+	for(int i=0;i<len;i++)
+	{
+		// That does to writing a char at terminal
+		ITM_SendChar((*ptr++));
+	}
+	return len;
+}
 
 TaskHandle_t myTask1Handle = NULL;
 TaskHandle_t myTask2Handle = NULL;
@@ -17,6 +30,10 @@ int main(void)
 	// Note: We should change 'configCPU_CLOCK_HZ', It'll be 16MHz in 'FreeRTOSConfig.h'
 
 	SystemCoreClockUpdate(); //-> It can do for changing the variable of 'SystemCoreClock' (So it'll be 16Mhz)
+
+	printf("Starting RTOS project.\n");
+	// You should to open "Serial Wire Viewer" in Debugger
+	// So you should to select port 0 in "Configure trace" in SWV Console
 
 	////// Creating a new task
 	xTaskCreate(Task1_Handler, "Görev 1", configMINIMAL_STACK_SIZE, NULL, 4, &myTask1Handle);
@@ -47,7 +64,8 @@ void Task1_Handler(void *params)
 {
 	for(;;) // While or For, Either is ok
 	{
-
+		printf("TASK - 1\n");
+		vTaskDelay(pdMS_TO_TICKS(100));
 	}
 }
 
@@ -55,7 +73,8 @@ void Task2_Handler(void *params)
 {
 	while(1) // While or For, Either is ok
 	{
-
+		printf("TASK - 2\n");
+		vTaskDelay(pdMS_TO_TICKS(100));
 	}
 }
 
